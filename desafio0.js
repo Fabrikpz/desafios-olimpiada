5e6a68c059ee1ec1e79b273ec9c9279ed6fd350a
Fabrikpz <fabrisilveyra@gmail.com>
2023-07-28 18:11:35 -0300

    ya anda.

--- desafio0.js
+++ desafio0.js
@@ -2,16 +2,18 @@ let auxkey = 23;
 let numero = 3;
 let keyToArr = String(auxkey).split('').map(Number);
 let keyLength = keyToArr.length;
-let frase = "abababccdzxy"; //resultado = cec ddd eff baa
+let frase = "YO HE LOGRADO ENCENDER UNA CERILLA"; //resultado = ar jh nr iu cg qh of gp fh tx od eh tl nñ c
 const abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 const test = (word, key, numero) => {
     let fraseRes = [];
-    let auxNumber = 0; let cont = 0;
-    let palabra = word.toLowerCase().split("");
+    let auxNumber = 0; 
+    let cont = 0;
+    let palabraSinEspacios = word.replace(/\s/g, '');
+    let palabra = palabraSinEspacios.toLowerCase().split("");
 
     palabra.map((letra) => {
-        let indice = parseInt((abc.indexOf(letra) + key[auxNumber]) % 27);
+        let indice = parseInt((abc.indexOf(letra) + key[auxNumber]) % 27); 
         fraseRes.push(abc[indice]);
         auxNumber++;
         cont++;
