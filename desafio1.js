7eb419ce1854e3ca02f95ae3502405194fe4a489
Fabrikpz <fabrisilveyra@gmail.com>
2023-07-29 15:12:43 -0300

    funca el desafio 1

--- desafio1.js
+++ desafio1.js
@@ -1,15 +1,26 @@
-function cadenas(a){
-    let divisores = [];
-
-    for(i = a; i >= 0; i--){
-        if(a % i === 0){
+const a = 60;
+let divisores = [];
+function obtenerDivisores(a) {
+    for (let i = a; i > 0; i--) {
+        if (a % i === 0) {
             divisores.push(i);
         }
     }
-    //return divisores;
-
-    let sum = [];
-    divisores.map(divisor => a % divisor ? sum.push[divisor] : "")
 }
 
-console.log(cadenas(60));
+obtenerDivisores(a); //60, 30, 20, 15, 12, 10, 6, 5, 4, 3, 2, 1
+
+function cadenas(a) {
+    let suma = [];
+
+    for (let i = 0; i < divisores.length; i++) {
+        if (a % divisores[i] === 0) {
+            suma.push(divisores[i]);
+            a = divisores[i];
+        }
+    }
+
+    return suma.reduce((n1, n2) => n1 + n2);
+}
+
+console.log(cadenas(a)); //resultado: 60+30+15+5+1 (111)
