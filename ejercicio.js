785efdcfd86a9ea7a21ae2b0d416e9fceaffb6cd
Fabrikpz <fabrisilveyra@gmail.com>
2023-08-03 20:52:32 -0300

    qsy

--- ejercicio.js
+++ ejercicio.js
@@ -1,5 +1,19 @@
-function ordenar(arr){
-    
+let index1 = 0;
+let index2 = 1;
+let cont = 0;
+function ordenar(arr, index1, index2, cont){
+    if(cont == arr.length-2){
+        return arr;
+    }
+
+    if(arr[index1] > arr[index2]){
+        aux = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = aux;
+        ordenar(arr, index1+1, index2-1, cont+1);
+    } else {
+        ordenar(arr, index1, index2+1, cont);
+    }
 }
 
-console.log(ordenar([2,3,-1,5]));
+console.log(ordenar([3,2,-1,5], index1, index2, cont));
