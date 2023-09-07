14ba94f670f64b918385f5403f249f48424ffc39
Fabrikpz <fabrisilveyra@gmail.com>
2023-09-07 16:58:33 -0300

    casi terminado

--- js/desafio5.js
+++ js/desafio5.js
@@ -1,22 +1,82 @@
 let matriz = [
-    [".", ".", ".", "<", ">", ".", ".", ".", "."],
-    ["<", ">", ".", ".", "<", ">", ".", "A", "."],
-    [".", ".", "<", ">", ".", "A", ".", "V", "."],
-    [".", ".", ".", ".", ".", "V", ".", ".", "."],
-    [".", "<", ">", ".", ".", ".", ".", ".", "."]
-]; // 5 x 9
-let si = [];
+    [".", ".", "<", ">", ".", ".", "A", "A"],
+    ["<", ">", "<", ">", "<", ">", "V", "V"],
+    [".", ".", ".", ".", ".", "A", ".", "."],
+    [".", ".", ".", ".", ".", "V", ".", "."],
+    [".", "<", ">", ".", ".", ".", ".", "."]
+]; // 5 x 8
 
-function gravitas(g){
-    for(y = 0; y < g.length; y++){
-        for(x = 0; x < g[i].length; x++){
-            //if(g[]){
-                si.push(g[y][x]);
-            //}
+function gravitas(g) {
+    for (y = g.length - 4; y >= 0; y--) {
+        for (x = 0; x < g[y].length; x++) {
+            if((g[y][x] == "<" || g[y][x] == ">") && g[y+1][x] == "." && g[y+1][x+1] == "."){
+                g[y+1][x] = g[y][x];
+                g[y][x] = ".";
+                g[y+1][x+1] = g[y][x+1];
+                g[y][x+1] = ".";
+                let auxY = y;
+                while(g[auxY][x] == "." && g[auxY][x+1] == "."){
+                    auxY+=1;
+                    g[auxY+1][x] = g[auxY][x];
+                    g[auxY][x] = ".";
+                    g[auxY+1][x+1] = g[auxY][x+1];
+                    g[auxY][x+1] = ".";
+                    if(g[auxY+1][x] != "." || g[auxY+1][x+1] != "."){
+                        break;
+                    }
+                }
+            }
+
+            if((g[y][x] == "V" || g[y][x] == "A" )&& g[y+1][x] == "."){
+                g[y+1][x] = g[y][x];
+                g[y][x] = g[y-1][x];
+                g[y-1][x] = ".";
+                let auxY2 = y;
+                while(g[auxY2][x] == "."){
+                    auxY2+=1;
+                    g[auxY2+1][x] = g[auxY2][x];
+                    g[auxY2][x] = g[auxY2-1][x];
+                    g[auxY2-1][x] = ".";
+                    if(g[auxY2+1][x] != "."){
+                        break;
+                    }
+                }
+            }
         }
     }
-
-    return g[4][1];
+    return g;
 }
 
-console.log(gravitas(matriz));
+function mostrarMatriz(matriz) {
+    for (let fila of matriz) {
+        console.log(fila.join(" "));
+    }
+}
+
+const resultado = gravitas(matriz);
+mostrarMatriz(resultado);
+/* let auxY = y;
+            if (g[y][x] == "<" || g[y][x] == ">" || g[y][x] == "V" || g[y][x] == "A") {
+                while (g[auxY + 1][x] == ".") {
+                    g[auxY + 1][x] = g[auxY][x];
+                    g[auxY][x] = ".";
+                    if (g[auxY][x + 1] == ">") {
+                        g[auxY][x] = g[auxY - 1][x];
+                        g[auxY - 1][x] = ".";
+                        auxY += 1;
+                        break;
+                    }
+                }
+                if (g[auxY + 1][x] == "V" || g[auxY + 1][x] == "<" || g[auxY + 1][x] == ">") {
+                    g[auxY][x] = g[auxY - 1][x];
+                    g[auxY - 1][x] = ".";
+                    auxY += 1;
+                }
+                if (g[auxY][x - 1] == "<" || g[auxY][x - 1] == ">") {
+                    break;
+                }
+                if (auxY >= g.length - 1) {
+                    auxY = y;
+                    break;
+                }
+            }*/
