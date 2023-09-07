f09b8d38908aa61f6635af5edc7b983fbfcfd505
Fabrikpz <fabrisilveyra@gmail.com>
2023-09-07 18:23:23 -0300

    for gravitas!!!

--- js/desafio5.js
+++ js/desafio5.js
@@ -3,49 +3,40 @@ let matriz = [
     ["<", ">", "<", ">", "<", ">", "V", "V"],
     [".", ".", ".", ".", ".", "A", ".", "."],
     [".", ".", ".", ".", ".", "V", ".", "."],
-    [".", "<", ">", ".", ".", ".", ".", "."]
+    [".", "<", ">", ".", "<", ">", ".", "."],
 ]; // 5 x 8
+const filas = matriz.length;
+const columnas = matriz[0].length;
 
 function gravitas(g) {
-    for (y = g.length - 4; y >= 0; y--) {
-        for (x = 0; x < g[y].length; x++) {
-            if((g[y][x] == "<" || g[y][x] == ">") && g[y+1][x] == "." && g[y+1][x+1] == "."){
-                g[y+1][x] = g[y][x];
-                g[y][x] = ".";
-                g[y+1][x+1] = g[y][x+1];
-                g[y][x+1] = ".";
-                let auxY = y;
-                while(g[auxY][x] == "." && g[auxY][x+1] == "."){
-                    auxY+=1;
-                    g[auxY+1][x] = g[auxY][x];
-                    g[auxY][x] = ".";
-                    g[auxY+1][x+1] = g[auxY][x+1];
-                    g[auxY][x+1] = ".";
-                    if(g[auxY+1][x] != "." || g[auxY+1][x+1] != "."){
-                        break;
-                    }
-                }
-            }
+    for (let y = filas - 2; y >= 0; y--) {
+        for (let x = 0; x < columnas; x++) {
+            let posActual = g[y][x];
+            const posAbajo = g[y + 1][x];
 
-            if((g[y][x] == "V" || g[y][x] == "A" )&& g[y+1][x] == "."){
-                g[y+1][x] = g[y][x];
-                g[y][x] = g[y-1][x];
-                g[y-1][x] = ".";
-                let auxY2 = y;
-                while(g[auxY2][x] == "."){
-                    auxY2+=1;
-                    g[auxY2+1][x] = g[auxY2][x];
-                    g[auxY2][x] = g[auxY2-1][x];
-                    g[auxY2-1][x] = ".";
-                    if(g[auxY2+1][x] != "."){
-                        break;
-                    }
+            if ((posActual == "V" || posActual == "A") && posAbajo == ".") {
+                let newY = y;
+                while (newY < filas - 1 && g[newY + 1][x] == ".") {
+                    g[newY + 1][x] = g[newY][x];
+                    g[newY][x] = ".";
+                    newY++;
+                }
+            } else if ((posActual == "<" || posActual == ">") && posAbajo == ".") {
+                let newY = y;
+                while (newY < filas - 1 && g[newY + 1][x] == "." && g[newY + 1][x + 1] == ".") {
+                    g[newY + 1][x] = g[newY][x];
+                    g[newY][x] = ".";
+                    g[newY + 1][x + 1] = g[newY][x + 1];
+                    g[newY][x + 1] = ".";
+                    newY++;
                 }
             }
         }
     }
+
     return g;
 }
+
 
 function mostrarMatriz(matriz) {
     for (let fila of matriz) {
@@ -55,28 +46,3 @@ function mostrarMatriz(matriz) {
 
 const resultado = gravitas(matriz);
 mostrarMatriz(resultado);
-/* let auxY = y;
-            if (g[y][x] == "<" || g[y][x] == ">" || g[y][x] == "V" || g[y][x] == "A") {
-                while (g[auxY + 1][x] == ".") {
-                    g[auxY + 1][x] = g[auxY][x];
-                    g[auxY][x] = ".";
-                    if (g[auxY][x + 1] == ">") {
-                        g[auxY][x] = g[auxY - 1][x];
-                        g[auxY - 1][x] = ".";
-                        auxY += 1;
-                        break;
-                    }
-                }
-                if (g[auxY + 1][x] == "V" || g[auxY + 1][x] == "<" || g[auxY + 1][x] == ">") {
-                    g[auxY][x] = g[auxY - 1][x];
-                    g[auxY - 1][x] = ".";
-                    auxY += 1;
-                }
-                if (g[auxY][x - 1] == "<" || g[auxY][x - 1] == ">") {
-                    break;
-                }
-                if (auxY >= g.length - 1) {
-                    auxY = y;
-                    break;
-                }
-            }*/
